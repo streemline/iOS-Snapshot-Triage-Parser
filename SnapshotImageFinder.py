645cf0a343186196c131826a9aa986b13eb8475f
Sourcery AI <>
2022-11-02 02:07:47 +0000

    'Refactored by Sourcery'

--- SnapshotImageFinder.py
+++ SnapshotImageFinder.py
@@ -20,14 +20,14 @@ data_dir = args.data_dir_to_analyze
 foldername = ('FoundSnapshotImages')
 pathfound = 0
 path = os.getcwd()
-outpath = path + "/" + foldername
+outpath = f"{path}/{foldername}"
 count = 0
-pathlog =[]	
+pathlog =[]
 os.makedirs(outpath)
 print("\n--------------------------------------------------------------------------------------")
 print("iOS Snapshot KTX file finder")
 print("By: Alexis Brignoni | @AlexisBrignoni | abrignoni.com")
-print("Source directory to be searched: " + data_dir)
+print(f"Source directory to be searched: {data_dir}")
 print("\n--------------------------------------------------------------------------------------")
 print("")
 print('Searching for iOS Snapshot images.')
@@ -41,28 +41,28 @@ for root, dirs, filenames in os.walk(data_dir):
 				pathlog.insert(count, pathfound)
 				#print(pathlog[count])
 				count = count + 1
-				
+
 			elif f.endswith('@2x.ktx'):
 				pathfound = os.path.join(root, f)
 				copy(pathfound, outpath)
 				pathlog.insert(count, pathfound)
 				#print(pathlog[count])
 				count = count + 1	
-			
+
 if pathfound == 0:
-	print('')
-	print('No snapshot ktx or png files available.')
+		print('')
+		print('No snapshot ktx or png files available.')
 else:
-	print('')
-	print('Snapshot ktx files moved to: '+ outpath)
-	print('Snapshot ktx files moved total: '+ str(count))
-	with open("SnapshotImageFinderPathsRreport.txt", "w") as output:
-		output.write('iOS Snapshot KTX File Finder \n')
-		output.write('By: Alexis Brignoni | @AlexisBrignoni | abrignoni.com \n')
-		output.write('Snapshot KTX files moved to: '+ outpath+'\n')
-		output.write('Snapshot ktx files moved total: '+ str(count)+'\n')
-		output.write('\n')
-		output.write('KTX file paths from source directory: \n')
-		for item in pathlog:
-			output.write('%s\n' % item)
-	print('Copy log at: '+path+'\SnapshotImageFinderPathsRreport.txt')		
+		print('')
+		print(f'Snapshot ktx files moved to: {outpath}')
+		print(f'Snapshot ktx files moved total: {str(count)}')
+		with open("SnapshotImageFinderPathsRreport.txt", "w") as output:
+				output.write('iOS Snapshot KTX File Finder \n')
+				output.write('By: Alexis Brignoni | @AlexisBrignoni | abrignoni.com \n')
+				output.write(f'Snapshot KTX files moved to: {outpath}' + '\n')
+				output.write(f'Snapshot ktx files moved total: {str(count)}' + '\n')
+				output.write('\n')
+				output.write('KTX file paths from source directory: \n')
+				for item in pathlog:
+					output.write('%s\n' % item)
+		print(f'Copy log at: {path}' + '\SnapshotImageFinderPathsRreport.txt')		
--- SnapshotTriage.py
+++ SnapshotTriage.py
@@ -31,8 +31,8 @@ pathfound = 0
 foldername = ("iOSSnapshotTriageReports_" + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
 
 #calculate timestamps
-unix = datetime.datetime(1970, 1, 1)  
-cocoa = datetime.datetime(2001, 1, 1)  
+unix = datetime.datetime(1970, 1, 1)
+cocoa = datetime.datetime(2001, 1, 1)
 delta = cocoa - unix 
 
 for root, dirs, filenames in os.walk(appState_dir):
@@ -42,39 +42,39 @@ for root, dirs, filenames in os.walk(appState_dir):
 
 
 if pathfound == 0:
-	print("No applicationState.db")
+		print("No applicationState.db")
 else:
-	path = os.getcwd()
-	try:  
-		outpath = path + "/" + foldername
-		os.mkdir(outpath)
-		os.mkdir(outpath+"/ExtractedBplistsFirstLevel")
-		os.mkdir(outpath+"/ExtractedBplistsSecondLevel")
-		os.mkdir(outpath+"/Reports")
-		os.mkdir(outpath+"/Reports/images")
-	except OSError:  
-		print("Error making directories")
-	
-	
-	
-	print("\n--------------------------------------------------------------------------------------")
-	print("iOS Snapshot Triage Parser.")
-	print("Objective: Triage iOS Snapshot images.")
-	print("By: Alexis Brignoni | @AlexisBrignoni | abrignoni.com")
-	print("Processed images directory: " + data_dir)
-	print("Snapshots database: " + appState_dir)
-	print("\n--------------------------------------------------------------------------------------")
-	print("")
-	
-	print("Database located at: "+pathfound)
-	print("Please wait...")
-	
-	#connect sqlite databases
-	#database = 'applicationState.db'
-	db = sqlite3.connect(pathfound)
-	cursor = db.cursor()
+		path = os.getcwd()
+		try:  
+				outpath = f"{path}/{foldername}"
+				os.mkdir(outpath)
+				os.mkdir(f"{outpath}/ExtractedBplistsFirstLevel")
+				os.mkdir(f"{outpath}/ExtractedBplistsSecondLevel")
+				os.mkdir(f"{outpath}/Reports")
+				os.mkdir(f"{outpath}/Reports/images")
+		except OSError:  
+			print("Error making directories")
 
-	cursor.execute('''SELECT
+
+
+		print("\n--------------------------------------------------------------------------------------")
+		print("iOS Snapshot Triage Parser.")
+		print("Objective: Triage iOS Snapshot images.")
+		print("By: Alexis Brignoni | @AlexisBrignoni | abrignoni.com")
+		print(f"Processed images directory: {data_dir}")
+		print(f"Snapshots database: {appState_dir}")
+		print("\n--------------------------------------------------------------------------------------")
+		print("")
+
+		print(f"Database located at: {pathfound}")
+		print("Please wait...")
+
+		#connect sqlite databases
+		#database = 'applicationState.db'
+		db = sqlite3.connect(pathfound)
+		cursor = db.cursor()
+
+		cursor.execute('''SELECT
 	application_identifier_tab.id,
 	application_identifier_tab.application_identifier,
 	kvs.value
@@ -84,136 +84,130 @@ else:
 	and key_tab.id = kvs.key
 	''')
 
-	all_rows = cursor.fetchall()
-	
-	for row in all_rows:
-		bundleid = row[1]
-		wbplist = row [2]
-		print('Processing: '+bundleid)
-		output_file = open('./'+foldername+'/ExtractedBplistsFirstLevel/'+bundleid+'.bplist', 'wb') #export from applicationState.db
-		output_file.write(wbplist)
-		output_file.close()
-		
-		g = open('./'+foldername+'/ExtractedBplistsFirstLevel/'+bundleid+'.bplist', 'rb')
-		plistg = ccl_bplist.load(g)
-		
-		output_file = open('./'+foldername+'/ExtractedBplistsSecondLevel/'+bundleid+'.bplist', 'wb') #export from applicationState.db
-		output_file.write(plistg)
-		output_file.close()
-		
-		g = open('./'+foldername+'/ExtractedBplistsSecondLevel/'+bundleid+'.bplist', 'rb')
-		plistg = ccl_bplist.load(g)
-		long = len(plistg['$objects'])
-		
-		#start report
-		h = open('./'+foldername+'/Reports/'+bundleid+'.html', 'w') #write report
-		h.write('<html><body>')
-		h.write('<h2>iOS Snapshots Triage Report </h2>')
-		h.write('<h3>Application: '+bundleid+'</h3>')
-		h.write('Data aggregated per following data source: '+pathfound)
-		h.write('<br/>')
-		h.write('Press on the image to get full size')
-		h.write('<br/>')
-		h.write ('<style> table, th, td {border: 1px solid black; border-collapse: collapse;}</style>')
-		h.write('<br/>')
-		
-		#opne table
-		h.write('<table>')
-		for i in range (0, long):
-			test = (plistg['$objects'][i])
-			try:
-				if test.endswith('@3x.ktx'):
-					h.write('<tr><td>Filename:<br><font size="3" color="green">'+str(test)+'</font></td></tr>')
-					h.write('<tr>')
-					h.write('<td>')
-					image = test.split('.')
-					imagenew = image[0]
-					path2 = os.getcwd()
-					imagepath = (path+'/'+data_dir+'/'+imagenew+'.png')
-					imageoutpath = (outpath+'/Reports/images')
-					#print ('path2: '+path2)
-					#print('image: '+imagepath)
-					#print('imagefinal: '+imageoutpath)
-					#print('foldername: '+foldername)
-					copy(imagepath, imageoutpath)
-					h.write('<a href=./images/'+imagenew+'.png target="_blank">')
-					h.write('<img src=./images/'+imagenew+'.png width="310" height="552" ')
-					h.write('/>')
-					h.write('</a>')
-					h.write('</td>')
-					h.write('</tr>')
-					#new html block
-					#convert the ktx to jpg and add to html
-					#print(test)
+		all_rows = cursor.fetchall()
 
-			except:
-				pass
-				
-			try:
-				if test.endswith('@2x.ktx'):
-					h.write('<tr><td>Filename:<br><font size="3" color="green">'+str(test)+'</font></td></tr>')
-					h.write('<tr>')
-					h.write('<td>')
-					image = test.split('.')
-					imagenew = image[0]
-					path2 = os.getcwd()
-					imagepath = (path+'/'+data_dir+'/'+imagenew+'.png')
-					imageoutpath = (outpath+'/Reports/images')
-					#print ('path2: '+path2)
-					#print('image: '+imagepath)
-					#print('imagefinal: '+imageoutpath)
-					#print('foldername: '+foldername)
-					copy(imagepath, imageoutpath)
-					h.write('<a href=./images/'+imagenew+'.png target="_blank">')
-					h.write('<img src=./images/'+imagenew+'.png width="310" height="552" ')
-					h.write('/>')
-					h.write('</a>')
-					h.write('</td>')
-					h.write('</tr>')
-					#new html block
-					#convert the ktx to jpg and add to html
-					#print(test)
-					#new html block
-					#convert the ktx to jpg and add to html
-				
-			except:
-				pass
-			
-			try:
-				if test.endswith('.png'):
-					h.write('<tr>')
-					h.write('<td>')
-					h.write('File not found on system. <br> <font size="3" color="green">'+str(test)+'</font>')
-					h.write('</td>')
-					h.write('</tr>')
-					#new html block
-					#convert the ktx to jpg and add to html
-					#print(test)
-				
-			except:
-				pass
-			
-			try:
-				if test['NS.time']:
-					dates = test['NS.time']
-					dia = str(dates)
-					dias = (dia.rsplit('.', 1)[0])
-					timestamp = datetime.datetime.fromtimestamp(int(dias)) + delta
-					
-					h.write('<tr>')
-					h.write('<td>')
-					h.write(str(timestamp))
-					h.write('</td>')
-					h.write('</tr>')
-					
-					#print(timestamp)
-					
-			except:
-				pass		
-		h.write('<table>')
-		h.write('<br/>')		
-		h.write('Script by: abrignoni.com')
-		h.write('</html>')
-		h.close()
-		count = count + 1
-print('Total of apps with processed snapshots: '+str(count))
+		for row in all_rows:
+				bundleid = row[1]
+				wbplist = row [2]
+				print(f'Processing: {bundleid}')
+				with open(f'./{foldername}/ExtractedBplistsFirstLevel/{bundleid}.bplist', 'wb') as output_file:
+						output_file.write(wbplist)
+				g = open(f'./{foldername}/ExtractedBplistsFirstLevel/{bundleid}.bplist', 'rb')
+				plistg = ccl_bplist.load(g)
+
+				with open(f'./{foldername}/ExtractedBplistsSecondLevel/{bundleid}.bplist', 'wb') as output_file:
+						output_file.write(plistg)
+				g = open(f'./{foldername}/ExtractedBplistsSecondLevel/{bundleid}.bplist', 'rb')
+				plistg = ccl_bplist.load(g)
+				long = len(plistg['$objects'])
+
+				with open(f'./{foldername}/Reports/{bundleid}.html', 'w') as h:
+						h.write('<html><body>')
+						h.write('<h2>iOS Snapshots Triage Report </h2>')
+						h.write(f'<h3>Application: {bundleid}</h3>')
+						h.write(f'Data aggregated per following data source: {pathfound}')
+						h.write('<br/>')
+						h.write('Press on the image to get full size')
+						h.write('<br/>')
+						h.write ('<style> table, th, td {border: 1px solid black; border-collapse: collapse;}</style>')
+						h.write('<br/>')
+
+						#opne table
+						h.write('<table>')
+						for i in range(long):
+								test = (plistg['$objects'][i])
+								try:
+										if test.endswith('@3x.ktx'):
+												h.write('<tr><td>Filename:<br><font size="3" color="green">'+str(test)+'</font></td></tr>')
+												h.write('<tr>')
+												h.write('<td>')
+												image = test.split('.')
+												imagenew = image[0]
+												path2 = os.getcwd()
+												imagepath = f'{path}/{data_dir}/{imagenew}.png'
+												imageoutpath = f'{outpath}/Reports/images'
+												#print ('path2: '+path2)
+												#print('image: '+imagepath)
+												#print('imagefinal: '+imageoutpath)
+												#print('foldername: '+foldername)
+												copy(imagepath, imageoutpath)
+												h.write(f'<a href=./images/{imagenew}' + '.png target="_blank">')
+												h.write(f'<img src=./images/{imagenew}' + '.png width="310" height="552" ')
+												h.write('/>')
+												h.write('</a>')
+												h.write('</td>')
+												h.write('</tr>')
+															#new html block
+															#convert the ktx to jpg and add to html
+															#print(test)
+
+								except:
+									pass
+
+								try:
+										if test.endswith('@2x.ktx'):
+												h.write('<tr><td>Filename:<br><font size="3" color="green">'+str(test)+'</font></td></tr>')
+												h.write('<tr>')
+												h.write('<td>')
+												image = test.split('.')
+												imagenew = image[0]
+												path2 = os.getcwd()
+												imagepath = f'{path}/{data_dir}/{imagenew}.png'
+												imageoutpath = f'{outpath}/Reports/images'
+												#print ('path2: '+path2)
+												#print('image: '+imagepath)
+												#print('imagefinal: '+imageoutpath)
+												#print('foldername: '+foldername)
+												copy(imagepath, imageoutpath)
+												h.write(f'<a href=./images/{imagenew}' + '.png target="_blank">')
+												h.write(f'<img src=./images/{imagenew}' + '.png width="310" height="552" ')
+												h.write('/>')
+												h.write('</a>')
+												h.write('</td>')
+												h.write('</tr>')
+															#new html block
+															#convert the ktx to jpg and add to html
+															#print(test)
+															#new html block
+															#convert the ktx to jpg and add to html
+
+								except:
+									pass
+
+								try:
+									if test.endswith('.png'):
+										h.write('<tr>')
+										h.write('<td>')
+										h.write('File not found on system. <br> <font size="3" color="green">'+str(test)+'</font>')
+										h.write('</td>')
+										h.write('</tr>')
+										#new html block
+										#convert the ktx to jpg and add to html
+										#print(test)
+
+								except:
+									pass
+
+								try:
+									if test['NS.time']:
+										dates = test['NS.time']
+										dia = str(dates)
+										dias = (dia.rsplit('.', 1)[0])
+										timestamp = datetime.datetime.fromtimestamp(int(dias)) + delta
+
+										h.write('<tr>')
+										h.write('<td>')
+										h.write(str(timestamp))
+										h.write('</td>')
+										h.write('</tr>')
+
+										#print(timestamp)
+
+								except:
+									pass
+						h.write('<table>')
+						h.write('<br/>')
+						h.write('Script by: abrignoni.com')
+						h.write('</html>')
+				count = count + 1
+print(f'Total of apps with processed snapshots: {str(count)}')
